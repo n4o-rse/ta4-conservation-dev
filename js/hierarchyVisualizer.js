3515e62e1eb105073e6a806da311346a1ebd880e
LasseMempel <lassemempel@gmail.com>
2024-07-23 07:01:58 +0200

    intended tree

--- js/hierarchyVisualizer.js
+++ js/hierarchyVisualizer.js
@@ -91,7 +91,7 @@ function validation([toppedData, idObject, doublettes, missingParents, ignored, 
       const radioDiv = document.createElement("div");
       radioDiv.id = "radioDiv";
       radioDiv.innerHTML = "Select visualization type: ";
-      const radioTypes = ["Tidy tree", "Cluster tree", "Radial tidy tree", "Radial cluster tree", "Collapsible Tree", "Force directed tree", "Sunburst"];
+      const radioTypes = ["Tidy tree", "Cluster tree", "Radial tidy tree", "Radial cluster tree", "Collapsible Tree", "Force directed tree", "Sunburst", "Intended Tree"];
       let lineBreakStarter = document.createElement("br");
       radioDiv.appendChild(lineBreakStarter);
       for (let i = 0; i < radioTypes.length; i++) {
@@ -159,6 +159,9 @@ function visualizeData([stratifiedData, idObject]) {
     }
     if (visualizationType == "Collapsible Tree") {
       svg = generateCollapsibleTree(stratifiedData, idObject);
+    }
+    if (visualizationType == "Intended Tree") {
+      svg = generateIntendedTree(stratifiedData, idObject);
     }
     //document.getElementById("chartDiv").innerHTML = svg.outerHTML;
     document.getElementById("errorText").innerHTML = "";
