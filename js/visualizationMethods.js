5e9a7100f2c354d1ec52d5acdb4c5788b33e5123
LasseMempel <lassemempel@gmail.com>
2024-08-12 16:27:09 +0200

    mhhm

--- js/visualizationMethods.js
+++ js/visualizationMethods.js
@@ -913,8 +913,8 @@ function generateIndentedComments(data, idObject) {
   }
 
   // add a on-click functionality for all nodes, displaying their id
-  node.on("click", function() {
-    alert(node.data.id);
+  node.on("click", function(d) {
+    alert(d.data.id);
   });
 
   return svg.node();
