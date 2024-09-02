fd3b7dfdfb72ba675cb244494b4628040caff2f9
LasseMempel <lassemempel@gmail.com>
2024-09-02 17:00:33 +0200

    securer py optimization

--- securer.py
+++ securer.py
@@ -29,6 +29,8 @@ annotations = []
 # find all subjects in the graph which are of type oa:Annotation and check them for multiple entries
 for s, p, o in g.triples((None, RDF.type, OA.Annotation)):
     annotations.append(s)
+multivalues = False
+multivalueAnnotations = []
 for annotation in annotations:
     # get the target of the annotation
     annotationTarget = list(g.objects(annotation, target))
@@ -37,10 +39,14 @@ for annotation in annotations:
     # get the created date of the annotation
     annotationCreated = list(g.objects(annotation, created))
     if len(annotationTarget) > 1 or len(annotationCreator) > 1 or len(annotationCreated) > 1:
-        # if there are multiple entries, cancel workflow of github action
-        print('Multiple entries found in annotation with subject: ' + str(annotation))
-        sys.exit(1)
-    else:
-        print('No multiple entries found in annotation with subject: ' + str(annotation))
-        sys.exit(0)
+        multivalues = True
+        multivalueAnnotations.append(annotation)
+
+# if there are multiple entries, cancel workflow of github action
+if multivalues:
+    print('Failure - Multiple entries found in annotations: ' + str(multivalueAnnotations))
+    sys.exit(1)
+else:
+    print('Success - No multiple entries found in annotations')
+    sys.exit(0)
 
