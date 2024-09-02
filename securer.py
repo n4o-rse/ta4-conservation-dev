83e1d64a79f3e7c91d4fafcf7c97546477e4bcba
LasseMempel <lassemempel@gmail.com>
2024-09-02 16:11:48 +0200

    securer.py searching for multiple values in annotations

--- securer.py
+++ securer.py
@@ -1,8 +1,36 @@
 import requests
+from rdflib import Graph, Literal, BNode, Namespace, URIRef
+from rdflib.namespace import SKOS, RDF, DC, DCTERMS, RDFS
 
+# check annotations for multiple entries
+OA = Namespace("http://www.w3.org/ns/oa#")
+creator = DCTERMS.creator
+created = DCTERMS.created
+target = OA.hasTarget
+# get the ttl file from the solid pod
 ttlText = requests.get('https://restaurierungsvokabular.solidweb.org/annotations/annotations.ttl').text
+# load ttlTest into a graph
+g = Graph()
+g.parse(data=ttlText, format='ttl')
+# load all annotations from the graph into list
+annotations = []
+# find all subjects in the graph which are of type oa:Annotation
+for s, p, o in g.triples((None, RDF.type, OA.Annotation)):
+    annotations.append(s)
+for annotation in annotations:
+    # get the target of the annotation
+    annotationTarget = list(g.objects(annotation, target))
+    # get the creator of the annotation
+    annotationCreator = list(g.objects(annotation, creator))
+    # get the created date of the annotation
+    annotationCreated = list(g.objects(annotation, created))
+    if len(annotationTarget) > 1 or len(annotationCreator) > 1 or len(annotationCreated) > 1:
+        raise Exception("Annotation with multiple targets, creators or created dates")
+        
+# check if the ttl file has changed
 with open('comments.ttl', 'r') as f:
     fileText = f.read()
+
 if fileText != ttlText:
     with open('comments.ttl', 'w') as f:
         f.write(ttlText)
