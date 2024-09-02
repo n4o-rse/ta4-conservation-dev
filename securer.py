8c7a01275f8da7964bb474e2036f5eaab7ab7bfe
LasseMempel <lassemempel@gmail.com>
2024-09-02 16:21:06 +0200

    commit new ttl before validating securer.py

--- securer.py
+++ securer.py
@@ -2,19 +2,30 @@ import requests
 from rdflib import Graph, Literal, BNode, Namespace, URIRef
 from rdflib.namespace import SKOS, RDF, DC, DCTERMS, RDFS
 
+# get the ttl file from the solid pod
+ttlText = requests.get('https://restaurierungsvokabular.solidweb.org/annotations/annotations.ttl').text
+
+# check if the ttl file has changed
+with open('comments.ttl', 'r') as f:
+    fileText = f.read()
+
+if fileText != ttlText:
+    with open('comments.ttl', 'w') as f:
+        f.write(ttlText)
+
 # check annotations for multiple entries
 OA = Namespace("http://www.w3.org/ns/oa#")
 creator = DCTERMS.creator
 created = DCTERMS.created
 target = OA.hasTarget
-# get the ttl file from the solid pod
-ttlText = requests.get('https://restaurierungsvokabular.solidweb.org/annotations/annotations.ttl').text
+
 # load ttlTest into a graph
 g = Graph()
 g.parse(data=ttlText, format='ttl')
 # load all annotations from the graph into list
 annotations = []
-# find all subjects in the graph which are of type oa:Annotation
+
+# find all subjects in the graph which are of type oa:Annotation and check them for multiple entries
 for s, p, o in g.triples((None, RDF.type, OA.Annotation)):
     annotations.append(s)
 for annotation in annotations:
@@ -25,13 +36,6 @@ for annotation in annotations:
     # get the created date of the annotation
     annotationCreated = list(g.objects(annotation, created))
     if len(annotationTarget) > 1 or len(annotationCreator) > 1 or len(annotationCreated) > 1:
+        # if there are multiple entries, raise an exception
         raise Exception("Annotation with multiple targets, creators or created dates")
-        
-# check if the ttl file has changed
-with open('comments.ttl', 'r') as f:
-    fileText = f.read()
-
-if fileText != ttlText:
-    with open('comments.ttl', 'w') as f:
-        f.write(ttlText)
 
