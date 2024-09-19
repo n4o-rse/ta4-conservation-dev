dd16c632697f74d37c6bc574514211eea43c141e
LasseMempel <lassemempel@gmail.com>
2024-09-19 12:44:51 +0200

    id creator updated for existing identifiers

--- idGenerator/generateID.py
+++ idGenerator/generateID.py
@@ -14,19 +14,28 @@ def createNewId(checkList):
         hasLetters = any(c.isalpha() for c in x)
         hasNumbers = any(c.isdigit() for c in x)
     return x
+
+def readOldIds():
+    with open("verwendeteIdentifier.csv", "r") as f:
+        # read file as csv file with first column "identifier" and return as list
+        oldIds = [row["identifier"] for row in csv.DictReader(f)]
+    return oldIds
         
+def main():
+    numberOfIds = 2000
+    idList = []
+    checkList = []
+    oldIds = readOldIds()
+    for i in range(len(oldIds)):
+        checkList.append(oldIds[i])
+    with open("idList.csv", "w") as f:
+        for i in range(numberOfIds):
+            ID = createNewId(checkList)
+            idList.append(ID)
+            checkList.append(ID)
+            f.write(ID + "\n")
+main()
 
-#print(idGenerator())
 
-numberOfIds = 2000
-idList = []
-checkList = []
-
-with open("idList.csv", "w") as f:
-    for i in range(numberOfIds):
-        ID = createNewId(checkList)
-        idList.append(ID)
-        checkList.append(ID)
-        f.write(ID + "\n")
 
                 
