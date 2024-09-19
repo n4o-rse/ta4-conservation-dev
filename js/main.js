4d01eea3e8225b7f865c09bb1a05efc1cb1c948c
LasseMempel <lassemempel@gmail.com>
2024-09-19 09:44:04 +0200

    way too many changes in too many places

--- js/main.js
+++ js/main.js
@@ -78,68 +78,80 @@ function collectThesaurusData(idObject, topPosition) {
   conceptSchemeFormButton.onclick = function() {generateThesaurus(idObject, topPosition)};
 }
 
-function setCommentURL() {
+function setConceptSchemeTitle() {
   event.preventDefault();
   // read and return value of global variable commentURL
-  commentURL = document.getElementById('commentURLInput').value;
-  return commentURL;
+  conceptSchemeTitle = document.getElementById('conceptSchemeTitle').value;
+  return conceptSchemeTitle;
 }
 
-async function readCommentaryFiles() {
-  folderGraphText = await readFromPod("https://restaurierungsvokabular.solidweb.org/annotations/", "text/turtle");
-  folderGraph = $rdf.graph();
-  // define LDP namespace
-  LDP = $rdf.Namespace("http://www.w3.org/ns/ldp#");
-  $rdf.parse(folderGraphText, folderGraph, "https://restaurierungsvokabular.solidweb.org/annotations/", "text/turtle");
-  // get values of all ldp:contains in n0: as a list
-  let folderList = folderGraph.each($rdf.sym("https://restaurierungsvokabular.solidweb.org/annotations/"), LDP("contains"));
-  // iterate over all values of ldp:contains
-  let annotationURLS = [];
-  let annotationMapper = {};
-
-  for (let x of folderList) {
-    // get the URL of the file
-    let fileURL = x.value;
-    // check if file ends with .ttl
-    if (fileURL.endsWith(".ttl")) {
-      // add URL to list of annotationURLS
-      annotationURLS.push(fileURL);
-    }
-    if (fileURL.endsWith("annotationMappings.json")) {
-      let jsonString = await readFromPod(fileURL, "application/json");
-      annotationMapper = JSON.parse(jsonString);
-    }
+async function readConceptSchemeTitles() {
+  annotationGraphText = await readFromPod(commentURL, "text/turtle");
+  annotationGraph = $rdf.graph();
+  // define namespaces
+  let SKOS = $rdf.Namespace("http://www.w3.org/2004/02/skos/core#");
+  let DCT = $rdf.Namespace("http://purl.org/dc/terms/");
+  $rdf.parse(annotationGraphText, annotationGraph, "text/turtle");
+  // get all conceptSchemes in graph
+  let conceptSchemes = annotationGraph.each(undefined, RDF("type"), SKOS("ConceptScheme"));
+  // get names of all conceptSchemes
+  let conceptSchemeNames = [];
+  for (let x of conceptSchemes) {
+    let conceptSchemeName = annotationGraph.any(x, DCT("title"));
+    conceptSchemeNames.push(conceptSchemeName.value);
   }
-  let commentURLSelector = document.getElementById("commentURLInput");
-  // add option tag to commentURLSelector for each URL in annotationURLS with value = URL and innerHTML = annotationMapper[URL]
-  for (let x of annotationURLS) {
+  // get selector for conceptSchemeTitle
+  let conceptSchemeTitleSelector = document.getElementById("conceptSchemeTitleInput");
+  // add generic option "keine"
+  let option = document.createElement("option");
+  option.value = "keine";
+  option.innerHTML = "keine";
+  conceptSchemeTitleSelector.appendChild(option);
+
+  // add option tag to commentURLSelector for each conceptSchemeTitle in conceptSchemeNames
+  for (let x of conceptSchemeNames) {
     let option = document.createElement("option");
     option.value = x;
-    option.innerHTML = annotationMapper[x];
-    commentURLSelector.appendChild(option);
+    option.innerHTML = x;
+    conceptSchemeTitleSelector.appendChild(option);
   }
 }
 
-async function createCommentURL() {
-  thesaurusName = document.getElementById('createCommentURLInput').value;
-  let commentURL = "https://restaurierungsvokabular.solidweb.org/annotations/" + thesaurusName + ".ttl";
-  writeToPod("", commentURL, "text/turtle");
-  /*
-  let commentMapper = "https://restaurierungsvokabular.solidweb.org/annotations/annotationMappings.json";
-  let commentMapperGraphText = await readFromPod(commentMapper, "application/json");
-  let commentMapperGraph = JSON.parse(commentMapperGraphText);
-  commentMapperGraph[commentURL] = thesaurusName;
-  let commentMapperGraphString = JSON.stringify(commentMapperGraph);
-  await writeToPod(commentMapper, commentMapperGraphString, "application/json");
-  */
-  
-
-
-
+async function createConceptScheme() {
+  newConceptSchemeTitle = document.getElementById('createconceptSchemeTitle').value;
+  // read annotation graph from pod
+  annotationGraphText = await readFromPod(commentURL, "text/turtle");
+  annotationGraph = $rdf.graph();
+  // define namespaces
+  let SKOS = $rdf.Namespace("http://www.w3.org/2004/02/skos/core#");
+  let DCT = $rdf.Namespace("http://purl.org/dc/terms/");
+  $rdf.parse(annotationGraphText, annotationGraph, "text/turtle");
+  // check if there already exists a ConceptScheme with title = newConceptSchemeTitle
+  let conceptSchemes = annotationGraph.each(undefined, RDF("type"), SKOS("ConceptScheme"));
+  for (let x of conceptSchemes) {
+    let conceptSchemeName = annotationGraph.any(x, DCT("title"));
+    if (conceptSchemeName.value == newConceptSchemeTitle) {
+      alert("Es existiert bereits ein Thesaurus mit diesem Titel!");
+      return;
+    }
+  }
+  // create new conceptScheme
+  let i = 1;
+  while (annotationGraph.any($rdf.sym("annotations/ConceptScheme" + i + "/"), RDF("type"), SKOS("ConceptScheme"))) {
+    i++;
+  }
+  let newConceptScheme = $rdf.sym("annotations/ConceptScheme" + i + "/");
+  annotationGraph.add(newConceptScheme, RDF("type"), SKOS("ConceptScheme"));
+  annotationGraph.add(newConceptScheme, DCT("title"), $rdf.lit(newConceptSchemeTitle));
+  // write annotation graph to pod
+  await writeToPod(annotationGraph, commentURL, "text/turtle");
+  // reload dropdown menu for conceptSchemes
+  readConceptSchemeTitles();
 }
 
 // global variables and event listeners
-let commentURL = "";
+let commentURL = "https://restaurierungsvokabular.solidweb.org/annotations/annotations2.ttl";
+let conceptSchemeTitle = "";
 
 const thesaurusFileInputForm = document.getElementById('fileForm');
 thesaurusFileInputForm.addEventListener('submit', thesaurusInputFile);
@@ -150,12 +162,12 @@ thesaurusUrlInputForm.addEventListener('submit', thesaurusInputUrl);
 const commentForm = document.getElementById("commentForm");
 commentForm.addEventListener("submit", updatePod);
 
-const commentURLForm = document.getElementById("commentURLForm");
-commentURLForm.addEventListener("submit", setCommentURL);
+const conceptSchemeTitleForm = document.getElementById("conceptSchemeTitleForm");
+conceptSchemeTitleForm.addEventListener("submit", setConceptSchemeTitle);
 
-const createCommentURLForm = document.getElementById("createCommentURLForm");
-createCommentURLForm.addEventListener("submit", createCommentURL);
+const createconceptSchemeTitleForm = document.getElementById("createconceptSchemeTitleForm");
+createconceptSchemeTitleForm.addEventListener("submit", createConceptScheme);
 
-readCommentaryFiles();
+readConceptSchemeTitles();
 
 
